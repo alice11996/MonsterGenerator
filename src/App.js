cdde59feda875ff1c5cec88ab93d2d99f2719347
Aakanksha Vats <vatsaakanksha11997@gmail.com>
2019-05-29 20:33:36 +0530

    mounted App.js, used jsonplaceholder instead of array file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React , {Component} from 'react';
 import CardList from './CardList';
-import{ monsters }from './monsters.js';
+// import{ monsters }from './monsters.js';
 import SearchBox from './SearchBox.js';
-
+import './App.css';
 
 class App extends Component{
 	constructor()
 	{	super()
 		this.state=
 		{
-			monsters: monsters,
+			monsters: [],
 			searchfield: ''
 
 		}
 
 
+
+	}
+
+
+	componentDidMount()
+	{	fetch('https://jsonplaceholder.typicode.com/users')
+
+
+			.then(response =>{
+
+				return response.json();
+
+
+	})
+
+		.then(users => {
+
+			this.setState({monsters:users});
+
+		})
+
+		
 
 	}
 
@@ -32,14 +54,22 @@ class App extends Component{
 	{	const filteredMonsters = this.state.monsters.filter(monsters=>{
 		return monsters.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 			})
-		return(
+
+	if (this.state.monsters.length ===0)
+	{	
+		return <h1 className='pa4 tc'> Loading </h1>
+
+	}
+	else{return(
 		<div className= 'tc'>
-		<h1> Monsters are Here </h1>
+		<h1 className='f1'> Monsters are Here </h1>
 		<SearchBox searchChange={this.onSearchChange}/>
 		<CardList monsters={filteredMonsters} />
 		</div>
 
 	);
+}
+		
 	}
 }
 export default App;
